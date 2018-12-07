02c9dce1a5e453da94ca1eb81205a09398136cba
RogersLei <793574772@qq.com>
2018-12-07 13:24:24 +0800

    upd eslit

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const koa = require( "koa" ),
-    logger = require( "./logger" ),
-    router = require( "./route" )
+  logger = require( "./logger" ),
+  router = require( "./route" )
 
 const app = new koa()
 
 app.use ( async ( ctx, next ) => {
-    console.log ( ctx.request.path )
+  console.log ( ctx.request.path )
 } )
 
 
-console.log( router )
 app.use( router.middleware() );
 
 app.listen( 8888, () => {
-    logger.info( "server listen on 8888" )
+  logger.info( "server listen on 8888" )
 } )
 
 // 设置定时任务
